95409835a85259057ea61e5763a5fa73e4c3592e
Nuzairb <nuzairb@gmail.com>
2019-04-28 20:55:26 +0500

    update

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,22 +1,27 @@
-const config = require('./config/website');
+const config = require('./config/website')
 
-const pathPrefix = config.pathPrefix === '/' ? '' : config.pathPrefix;
+const pathPrefix = config.pathPrefix === '/' ? '' : config.pathPrefix
 
 module.exports = {
   /* General Information */
-  pathPrefix: config.pathPrefix,
   siteMetadata: {
     siteUrl: config.siteUrl + pathPrefix,
   },
   /* Plugins */
   plugins: [
     'gatsby-plugin-react-helmet',
-    'gatsby-plugin-emotion',
+    'gatsby-plugin-styled-components',
     {
       resolve: 'gatsby-source-filesystem',
       options: {
         path: `${__dirname}/src/images/`,
         name: 'images',
+      },
+    },
+    {
+      resolve: 'gatsby-plugin-google-analytics',
+      options: {
+        trackingId: config.googleAnalyticsID,
       },
     },
     'gatsby-transformer-sharp',
@@ -25,12 +30,12 @@ module.exports = {
       resolve: 'gatsby-plugin-manifest',
       options: {
         name: config.siteTitle,
-        short_name: config.siteTitleAlt,
+        short_name: config.siteTitleShort,
         description: config.siteDescription,
         start_url: config.pathPrefix,
         background_color: config.backgroundColor,
         theme_color: config.themeColor,
-        display: 'fullscreen',
+        display: 'standalone',
         icons: [
           {
             src: '/favicons/android-chrome-192x192.png',
@@ -49,4 +54,4 @@ module.exports = {
     'gatsby-plugin-offline',
     'gatsby-plugin-netlify',
   ],
-};
+}
--- src/components/SVG.jsx
+++ src/components/SVG.jsx
@@ -1,17 +1,20 @@
-/* global tw */
-import React from 'react';
-import PropTypes from 'prop-types';
-import styled from 'react-emotion';
-import { width as twWidth } from '../../tailwind';
+import React from 'react'
+import PropTypes from 'prop-types'
+import styled from 'styled-components'
+import tw from 'tailwind.macro'
+import { width as twWidth } from '../../tailwind'
+import { hidden } from '../styles/utils'
 
 const Wrapper = styled.svg`
-  ${tw('stroke-current absolute')};
+  ${tw`absolute`};
+  stroke: currentColor;
+  ${props => props.hiddenMobile && hidden};
   color: ${props => props.stroke};
   width: ${props => props.svgWidth};
   fill: ${props => props.fill};
   left: ${props => props.left};
   top: ${props => props.top};
-`;
+`
 
 const icons = {
   triangle: {
@@ -33,19 +36,19 @@ const icons = {
   },
   arrowUp: {
     shape: (
-      <React.Fragment>
+      <>
         <path d="M28.74,20.81H1.26a1.26,1.26,0,0,1-1-2L14.16.5a1.25,1.25,0,0,1,1-.5h0a1.24,1.24,0,0,1,1,.51L29.75,18.8a1.25,1.25,0,0,1-1,2ZM3.81,18.29H26.22L15.16,3.37Z" />{' '}
         <path d="M28.74,42H1.26a1.28,1.28,0,0,1-1.13-.71A1.26,1.26,0,0,1,.26,40l13.9-18.29a1.28,1.28,0,0,1,1-.5h0a1.24,1.24,0,0,1,1,.51L29.75,40a1.26,1.26,0,0,1,.12,1.32A1.28,1.28,0,0,1,28.74,42ZM3.81,39.47H26.22L15.16,24.55Z" />
-      </React.Fragment>
+      </>
     ),
     viewBox: '0 0 30 42',
   },
   upDown: {
     shape: (
-      <React.Fragment>
+      <>
         <path d="M28.74,44.58a1.28,1.28,0,0,1-1-.51L15.16,27.13l-12.89,17a1.26,1.26,0,1,1-2-1.53l13.9-18.29a1.34,1.34,0,0,1,1-.5,1.24,1.24,0,0,1,1,.51L29.75,42.56a1.27,1.27,0,0,1-1,2Z" />
         <path d="M14.83,20.82h0a1.28,1.28,0,0,1-1-.52L.25,2a1.27,1.27,0,0,1,2-1.51L14.84,17.45,27.73.5a1.26,1.26,0,0,1,2,1.53L15.84,20.32A1.28,1.28,0,0,1,14.83,20.82Z" />
-      </React.Fragment>
+      </>
     ),
     viewBox: '0 0 30 44.58',
   },
@@ -65,9 +68,18 @@ const icons = {
     ),
     viewBox: '0 0 30 30',
   },
-};
+  cross: {
+    shape: (
+      <path
+        strokeWidth="3px"
+        d="M60.5,50l34.8-34.8c2.9-2.9,2.9-7.6,0-10.5c-2.9-2.9-7.6-2.9-10.5,0L50,39.5L15.2,4.7c-2.9-2.9-7.6-2.9-10.5,0    c-2.9,2.9-2.9,7.6,0,10.5L39.5,50L4.7,84.8c-2.9,2.9-2.9,7.6,0,10.5c1.4,1.4,3.3,2.2,5.2,2.2s3.8-0.7,5.2-2.2L50,60.5l34.8,34.8    c1.4,1.4,3.3,2.2,5.2,2.2c1.9,0,3.8-0.7,5.2-2.2c2.9-2.9,2.9-7.6,0-10.5L60.5,50z"
+      />
+    ),
+    viewBox: '0 0 100 100',
+  },
+}
 
-const SVG = ({ stroke, fill, width, icon, left, top, className }) => (
+const SVG = ({ stroke, fill, width, icon, left, top, hiddenMobile }) => (
   <Wrapper
     viewBox={icons[icon].viewBox}
     stroke={stroke}
@@ -75,23 +87,23 @@ const SVG = ({ stroke, fill, width, icon, left, top, className }) => (
     svgWidth={twWidth[`${width}`]}
     left={left}
     top={top}
-    className={className}
+    hiddenMobile={hiddenMobile}
   >
     {icons[icon].shape}
   </Wrapper>
-);
+)
 
-export default SVG;
+export default SVG
 
 SVG.propTypes = {
   stroke: PropTypes.string,
   fill: PropTypes.string,
   width: PropTypes.number,
-  icon: PropTypes.oneOf(['triangle', 'circle', 'arrowUp', 'upDown', 'box', 'hexa']).isRequired,
+  icon: PropTypes.oneOf(Object.keys(icons)).isRequired,
   left: PropTypes.string,
   top: PropTypes.string,
-  className: PropTypes.string,
-};
+  hiddenMobile: PropTypes.bool,
+}
 
 SVG.defaultProps = {
   stroke: 'transparent',
@@ -99,5 +111,5 @@ SVG.defaultProps = {
   fill: 'none',
   left: '0%',
   top: '0%',
-  className: 'fancy-icon',
-};
+  hiddenMobile: false,
+}
